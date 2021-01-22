aee7cad8438c72861de40d4a4215b026e950e00d
vr100 <vr100@users.noreply.github.com>
2021-01-23 03:44:03 +0530

    run prediction in batches for regression script

--- scripts/utils/regression.py
+++ scripts/utils/regression.py
@@ -4,6 +4,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 from . import metrics as metrics_util
+import numpy as np
 
 def get_model(reg_type):
 	if reg_type == "linear":
@@ -23,6 +24,23 @@ def get_model(reg_type):
 def train(model, x, y):
 	model.fit(x, y)
 	return model
+
+def batch_evaluate(model, x, y, metrics):
+	print("predicting..")
+	y_pred = np.empty(shape=(0, y.shape[1]))
+	start = 0
+	batch_size = 1024
+	while start < len(x):
+		print("Evaluating {}".format(start))
+		end = (start + batch_size) if (start + batch_size) < len(x) else len(x)
+		x_batch = x[start:end]
+		output = model.predict(x_batch)
+		y_pred = np.append(y_pred, output)
+		start = end
+	print("getting metrics..")
+	metrics_info = metrics_util.compute_metrics(y, y_pred, metrics,
+		multioutput="raw_values")
+	return (y_pred, metrics_info)
 
 def evaluate(model, x, y, metrics):
 	print("predicting..")
