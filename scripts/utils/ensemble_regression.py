ca3aa43c112bfb123fe9986ca7fa0245826ee4fc
vr100 <vr100@users.noreply.github.com>
2021-02-16 01:25:42 +0530

    using multi output regression

--- scripts/utils/ensemble_regression.py
+++ scripts/utils/ensemble_regression.py
@@ -3,6 +3,7 @@ import utils.misc as helper
 import utils.metrics as metrics_util
 from sklearn.ensemble import VotingRegressor, AdaBoostRegressor, StackingRegressor, BaggingRegressor
 import numpy as np
+from sklearn.multioutput import MultiOutputRegressor
 
 def _get_estimators(x_size, y_size, config):
 	estimator_count = config["estimator_count"]
@@ -24,25 +25,29 @@ def get_model(x_size, y_size, config):
 	ensemble_type = config["ensemble_type"]
 	if ensemble_type == "voting":
 		estimators = _get_estimators(x_size, y_size, config)
-		return VotingRegressor(estimators)
+		model = VotingRegressor(estimators)
 	elif ensemble_type == "adaboost":
 		base_estimator = regression.get_model(x_size, y_size, config)
-		return AdaBoostRegressor(base_estimator=base_estimator,
+		model = AdaBoostRegressor(base_estimator=base_estimator,
 			n_estimators=config["estimator_count"], random_state=0)
 	elif ensemble_type == "stacking":
 		estimators = _get_estimators(x_size, y_size, config)
-		return StackingRegressor(estimators=estimators, cv=10,
+		model = StackingRegressor(estimators=estimators, cv=config["cv"],
 			n_jobs=config["job_count"], passthrough=True,
 			verbose=1)
 	elif ensemble_type == "bagging":
 		base_estimator = regression.get_model(x_size, y_size, config)
-		return BaggingRegressor(base_estimator=base_estimator,
+		model = BaggingRegressor(base_estimator=base_estimator,
 			n_estimators=config["estimator_count"],
 			n_jobs=config["job_count"], random_state=0,
 			verbose=1)
 	else:
 		print("Unknown ensemble {}".format(ensemble_type))
 		exit()
+	if y_size > 1:
+		return MultiOutputRegressor(model)
+	else:
+		return model
 
 def train(model, x, y):
 	if not isinstance(x, np.ndarray):
